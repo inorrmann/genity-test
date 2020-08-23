f6c64172d91800d5b0e757082c7a73364a2beba0
Ingrid Norrmann-Vigil <inorrmann@gmail.com>
2020-08-23 01:35:59 -0700

    Updates

---
+++ precache-manifest.bafad74a8609adb5151dcffd1d0771e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0996bda6ae8a3e0dc8dcf3ec897dae",
+    "url": "/genity-test/index.html"
+  },
+  {
+    "revision": "a72fb2943656d35afd8a",
+    "url": "/genity-test/static/css/main.da6f1b66.chunk.css"
+  },
+  {
+    "revision": "3a020481e5b63f39a937",
+    "url": "/genity-test/static/js/2.01c22366.chunk.js"
+  },
+  {
+    "revision": "8b81bc38395cca664a526c4e304a6603",
+    "url": "/genity-test/static/js/2.01c22366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72fb2943656d35afd8a",
+    "url": "/genity-test/static/js/main.8344191c.chunk.js"
+  },
+  {
+    "revision": "e789fe6c62c7d3d62fd0",
+    "url": "/genity-test/static/js/runtime-main.6981ba25.js"
+  },
+  {
+    "revision": "1d23af983a98bd292d84da613903ccd2",
+    "url": "/genity-test/static/media/database-solid.1d23af98.svg"
+  },
+  {
+    "revision": "cf5e4039d88b7054bd1ac282e861a537",
+    "url": "/genity-test/static/media/file-alt-solid.cf5e4039.svg"
+  },
+  {
+    "revision": "92c7d580d983bc1ba5c9c0a85b898e8c",
+    "url": "/genity-test/static/media/server-solid.92c7d580.svg"
+  },
+  {
+    "revision": "2e1d2b477aab5be4df3d5de3644ac143",
+    "url": "/genity-test/static/media/settings-ethernet.2e1d2b47.svg"
+  }
+]);
